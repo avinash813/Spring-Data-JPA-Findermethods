8ca73aad79751c36bbf77c5b9e9310fd9cbef3a4
avinash813 <113618966+avinash813@users.noreply.github.com>
2024-06-18 21:54:18 +0530

    SpringBoot | findBy methods

--- src/main/java/com/sample/service/IVaccineManagement.java
+++ src/main/java/com/sample/service/IVaccineManagement.java
@@ -10,5 +10,14 @@ import com.sample.view.View;
 public interface IVaccineManagement 
 {
 	public  <T extends View>List<T> fetchByCountry(String vaccineName, Class<T> cls);
+
+	public List<VaccineInfo> searchByVaccinePrice(Integer price);
+	
+	 public List<VaccineInfo> searchByCountryName(String countryName);
+	 
+	 public List<VaccineInfo> searchByPriceLessThan(Integer price);
+     
+	   public List<VaccineInfo> searchByVaccineNameInAndPriceBetween(Collection<String> vaccines, Integer 
+			   staringPrice, Integer endPrice);
 	
 }
